2fee5f3cecae23bd8fb0ab707b0bcd7c710271ac
Meir Soskil <102519952+Meisosk@users.noreply.github.com>
2023-09-13 12:51:03 -0400

    Add files via upload

---
+++ styles/index.js
@@ -0,0 +1,34 @@
+const listItems = document.querySelectorAll('.nav ul li');
+const sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    const top = window.scrollY;
+
+    // Handle the "Welcome" link separately
+    if (top === 0) {
+        listItems.forEach(item => {
+            item.classList.remove('active');
+        });
+        listItems[0].classList.add('active'); // Assuming "Welcome" is the first item
+        return;
+    }
+
+    // Handle other sections
+    sections.forEach(sec => {
+        const offset = sec.offsetTop - 150
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+        const navLink = document.querySelector('.nav ul li a[href="#' + id + '"]');
+
+        if (navLink && top >= offset && top < offset + height) {
+            listItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            navLink.parentElement.classList.add('active');
+        }
+    });
+};
+
+
+
+
