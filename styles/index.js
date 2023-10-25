6b08e4d31d8fdb92f26c490e22461345861f5fa3
Meir Soskil <meirsoskil@gmail.com>
2023-10-25 17:44:26 -0400

    added project pics and email works

--- styles/index.js
+++ styles/index.js
@@ -1,34 +1,30 @@
-const listItems = document.querySelectorAll('.nav ul li');
-const sections = document.querySelectorAll('section');
+const listItems = document.querySelectorAll(".nav ul li");
+const sections = document.querySelectorAll("section");
 
 window.onscroll = () => {
-    const top = window.scrollY;
+  const top = window.scrollY;
 
-    // Handle the "Welcome" link separately
-    if (top === 0) {
-        listItems.forEach(item => {
-            item.classList.remove('active');
-        });
-        listItems[0].classList.add('active'); // Assuming "Welcome" is the first item
-        return;
-    }
-
-    // Handle other sections
-    sections.forEach(sec => {
-        const offset = sec.offsetTop - 150
-        const height = sec.offsetHeight;
-        const id = sec.getAttribute('id');
-        const navLink = document.querySelector('.nav ul li a[href="#' + id + '"]');
-
-        if (navLink && top >= offset && top < offset + height) {
-            listItems.forEach(item => {
-                item.classList.remove('active');
-            });
-            navLink.parentElement.classList.add('active');
-        }
+  // Handle the "Welcome" link separately
+  if (top === 0) {
+    listItems.forEach((item) => {
+      item.classList.remove("active");
     });
+    listItems[0].classList.add("active"); // Assuming "Welcome" is the first item
+    return;
+  }
+
+  // Handle other sections
+  sections.forEach((sec) => {
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+    const navLink = document.querySelector('.nav ul li a[href="#' + id + '"]');
+
+    if (navLink && top >= offset && top < offset + height) {
+      listItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      navLink.parentElement.classList.add("active");
+    }
+  });
 };
-
-
-
-
